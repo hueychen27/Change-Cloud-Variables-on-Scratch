f474f3add1311d9d3242deeffbbfc171102e278d
Da_Person <hueychen27@outlook.com>
2024-03-09 23:16:33 -0800

    Add option for config file

---
+++ README.txt
@@ -0,0 +1,15 @@
+# How to use
+Run `python3 script.py`  
+Answer the questions in the Python file.  
+See https://github.com/TimMcCool/scratchattach/wiki/Get-your-session-id to get session id on Scratch.
+# What is the JSON file here?
+The JSON file configures the credentials and also if the cloud variable will be set forever.  
+Modify the values like this:  
+```
+{
+    "use": [Do you want to use this config file?: true or false],
+    "username": [Input Scratch username: your Scratch username],
+    "sessionId": [Input session id: session id (See https://github.com/TimMcCool/scratchattach/wiki/Get-your-session-id for session id)],
+    "forever": [Run the script forever?: true or false]
+}
+```
--- script.py
+++ script.py
@@ -1,15 +1,32 @@
+#!/bin/env python3
+from multiprocessing import forkserver
 import os
 
 os.system("pip install -U scratchattach --break-system-packages")
 
 import scratchattach as scratch3
+import json
 
-id=input("Input session id (See https://github.com/TimMcCool/scratchattach/wiki/Get-your-session-id to get cookie id): ")
-username=input("Input your Scratch username: ")
+f = open("config.json", "r")
+conf=json.loads(f.read())
+global username
+global id
+global forever
+if conf["use"]==True:
+    username = conf["username"]
+    id=conf["sessionId"]
+    forever=conf["forever"]
+else:
+    id=input("Input session id (See https://github.com/TimMcCool/scratchattach/wiki/Get-your-session-id to get cookie id): ")
+    username=input("Input your Scratch username: ")
+f.close()
 project_id=input("Input project id: ")
 conn = scratch3.CloudConnection(project_id = project_id, username=username, session_id=id)
 print("Logged in!")
 var_name=input("What variable name do you want to set?: ")
 var_input=input("What do you want to set " + var_name + " to?: ")
-conn.set_var(var_name, var_input)
-print("Done!")
+if forever==True:
+    while True:
+        conn.set_var(var_name, var_input)
+else:
+    conn.set_var(var_name, var_input)
